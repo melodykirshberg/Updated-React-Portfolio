44ea0edd6090a9641546dca1cda6b946ed5de635
verlitas <55808608+verlitas@users.noreply.github.com>
2020-01-29 12:10:52 -0700

    trying

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 8080;
 const path = require("path");
 
 app.use(express.static("build"));
-
 app.get("*", function (req, res) {
     res.sendFile(path.resolve(__dirname, "build/index.html"));
 });
