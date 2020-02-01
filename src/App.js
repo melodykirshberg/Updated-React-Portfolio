38c6fd13ac8049fe6c6589c8d2220514161dead3
verlitas <55808608+verlitas@users.noreply.github.com>
2020-01-31 17:13:39 -0700

    fixed missing import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import About from './components/About/About.js'
+import Nav from './components/Nav/Nav.js';
+import About from './components/About/About.js';
 import Portfolio from './components/Portfolio/Portfolio.js';
 import Contact from './components/Contact/Contact.js';
 import Footer from './components/Footer/Footer.js';
