0a67e1195c99c8cc5183ec3dda8610212b949bbe
verlitas <55808608+verlitas@users.noreply.github.com>
2020-02-03 10:13:56 -0700

    updating with brandstatement

--- src/components/About/Data.js
+++ src/components/About/Data.js
@@ -2,6 +2,6 @@ import Self from './Self.jpg';
 
 export default {
     header: 'Hi my name is Melody and I am a full stack developer.',
-    bio: 'Full Stack Web Developer with a passion to create artistic and visually unique websites for clients. Known for creating unique functionality as well as design for apps.',
+    bio: 'Full Stack Web Developer with a passion to create artistic and visually unique websites for clients. Graduated with a certificate in Full Stack Web Development from the University of Arizona with skills in JavaScript, React.js, Node.js, CSS, and HTML. Known for creating visually unique ideas as well as going out of the way to design apps by willingly learning new technologies. Strengths in efficiently collaborating in group settings, taking the lead when there is no sense of direction, communicating effectively to fellow coworkers when needing clarification and a set mentality of trying new things. Recently I worked on the (back end of a project using Sequelize and mySQL, as well as contributed my artistic skills to create custom icons with Adobe Photoshop for the front end.)',
     image: Self
 }
