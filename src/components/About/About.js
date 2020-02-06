b5768f89acbe7e9cbf8563586b8bb3a8258ef29d
verlitas <55808608+verlitas@users.noreply.github.com>
2020-02-06 07:15:39 -0700

    added availability and fixed clearfix

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './About.css'
+import Self from './Self.jpg';
 import Data from './Data.js';
 import Fade from 'react-reveal/Fade';
 
@@ -14,10 +15,11 @@ class About extends Component {
                     </Fade>
                     <Fade bottom>
                         <h3>{Data.header}</h3>
-                        <Fade bottom>
-                            <img src={Data.image} alt='Self'></img>
-                        </Fade>
+                        <img src={Self} alt='Self'></img>
                         <p>{Data.bio}</p>
+                        <div className='available'>
+                            I am currently <a href='mailto:melodykirshberg@gmail.com' className='open'> open </a>for projects
+                        </div>
                     </Fade>
                 </div>
             </div>);
--- src/components/About/Data.js
+++ src/components/About/Data.js
@@ -1,7 +1,7 @@
 import Self from './Self.jpg';
 
 export default {
-    header: 'Hi my name is Melody',
+    header: 'Hello, my name is Melody Kirshberg',
     bio: 'Front-end/Full Stack Web Developer with a passion to create artistic and visually unique websites for clients. Graduated with a certificate in Full Stack Web Development from the University of Arizona with skills in JavaScript, React.js, Node.js, CSS, and HTML. Known for creating unique ideas, taking an open approach to trying new things, as well as conscientious design for apps. Strengths include efficiently collaborating in group settings, taking the lead when there is no sense of direction, and communicating effectively to fellow coworkers when needing clarification. Recently I worked on the back end of a project using Sequelize and mySQL, as well as contributed my artistic skills to create custom icons with Adobe Photoshop for the front end.',
     image: Self
 }
